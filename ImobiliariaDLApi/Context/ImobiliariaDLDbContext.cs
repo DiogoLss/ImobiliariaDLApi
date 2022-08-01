51f646ea9e90b1ee396d1def7f146f6d223fc5ec
Diogo Lessa <diogodslessa@gmail.com>
2022-08-01 17:16:42 -0300

    Context

---
+++ ImobiliariaDLApi/Context/ImobiliariaDLDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ImobiliariaDLApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImobiliariaDLApi.Context
+{
+    public class ImobiliariaDLDbContext : DbContext
+    {
+        public ImobiliariaDLDbContext(DbContextOptions<ImobiliariaDLDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Imovel>? Imoveis { get; set; }
+        public DbSet<Imagem>? Imagens { get; set; }
+        public DbSet<Endereco>? Endereco { get; set; }
+    }
+}
